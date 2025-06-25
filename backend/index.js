c4fd9e7be33b29eb8e95fc101b3797e64390e2be
max925001 <shivam969684@gmail.com>
2025-06-25 21:47:37 +0530

    speedup render

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import clothrouter from './routes/cloths.route.js'
+import axios from 'axios'
 
 const app = express()
 const PORT = process.env.PORT || 5001
@@ -29,6 +30,25 @@ app.use(cors({
 app.use(cookieParser())
 
 app.use(morgan('dev'))
+
+const url = "https://clothstore-v65v.onrender.com/ping"
+const interval = 300000;
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 app.get('/ping', (req, res) => {
     res.send('pong')
 })
